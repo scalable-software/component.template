606111f0531b3fd635de8b3dbf45881f1e0dc671
Engineer <software.engineer@scalable.software>
2024-10-01 11:48:33 -0700

    MODULE: Update Template

--- src/Pin.ts
+++ src/Pin.ts
@@ -3,10 +3,20 @@
  */
 import { Component } from "@scalable.software/component";
 
+import { Tag } from "./Index.js";
+
 /**
  * A pin button that can be:
  * 1. pinned and unpinned
  * 2. hidden and shown
  * @category Components
  */
-export class Pin extends Component {}
+export class Pin extends Component {
+  /**
+   * The tag name of the component
+   * @category Configuration
+   */
+  public static get Tag() {
+    return Tag;
+  }
+}
